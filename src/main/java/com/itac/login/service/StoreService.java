071110066aa564471b7b267c2bd450a26493ab31
scullystory <kyh0124.12@gmail.com>
2024-02-17 13:11:27 +0900

    추천 사용자 정의 함수 뼈대생성, StoreEntity 수정전

--- src/main/java/com/itac/login/service/StoreService.java
+++ src/main/java/com/itac/login/service/StoreService.java
@@ -6,8 +6,10 @@ import com.itac.login.entity.store.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -21,11 +23,9 @@ public class StoreService {
     }
 
     public List<Store> searchWithWord(String searchWord) {
-
         return storeRepository.findByStoreNameContaining(searchWord);
     }
 
-    @GetMapping("/location/{locationWord}")
     public List<Store> searchWithLocation(String locationWord){
         return storeRepository.findByStoreLocationContaining(locationWord);
     }
@@ -62,6 +62,47 @@ public class StoreService {
             return true;
         }
         return false;
+    }
 
+    private List<Store> recommend(List<Store> list){
+        final int gradeWeight = 1;
+        HashMap<Store,Integer> map = new HashMap<Store,Integer>();
+        
+        //별점순으로 점수 매겨주기
+        list = sortByGrade(list);
+        for(Store store:list){
+            map.put(store,map.getOrDefault(store,0)+
+                    ((list.size()-1)-list.indexOf(store))/(list.size()-1)*100*gradeWeight);
+        }
+
+        //리뷰 많은순 처리 필요
+        //list =
+
+        //가격 낮은순으로 처리 필요
+
+        //거리 순으로 처리 필요
+
+        //처리된 결과에 따라 list 재정렬
+
+        Collections.sort(list,new Comparator<Store>(){
+            @Override
+            public int compare(Store o1, Store o2) {
+                return map.get(o2)-map.get(o1);
+            }
+        });
+
+        return list;
+    }
+
+    //점수 높은 순(내림차순)으로 정렬, 2번째 매개변수가 있을경우에는 (오름차순)으로 정렬
+    private List<Store> sortByGrade(List<Store> list,Object ...flag){
+
+        final int orderDirection = (flag.length==0?-1:1); //내림차순일 때 -1, 오름차순일때 1
+
+        list.sort((o1, o2) ->{
+            return orderDirection*(int)(Float.parseFloat(o1.getGrade())-Float.parseFloat(o2.getGrade()));
+        } );
+
+        return list;
     }
 }
