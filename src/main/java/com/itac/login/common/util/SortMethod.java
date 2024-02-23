29ca467e69c17cf69caad2d36f2e42bba1d6acc0
kyh012412 <kyh0124.12@gmail.com>
2024-02-23 16:23:41 +0900

    브랜치 병합전 커밋

--- src/main/java/com/itac/login/common/util/SortMethod.java
+++ src/main/java/com/itac/login/common/util/SortMethod.java
@@ -22,7 +22,6 @@ public class SortMethod {
     @Autowired
     ReviewRepository reviewRepository;
 
-
     public List<Store> recommendfy(List<Store> list){
         final int gradeWeight = 100,
                 reviewWeight=100,
@@ -32,23 +31,21 @@ public class SortMethod {
 
         //별점순으로 점수 매겨주기
         list = sortByGrade(list);
-        
         //for문 내부의 Hashmap을 바꾸는 부분은 아래 메서드가 추가 구현되면 반복되는 구문이라서 메서드화예정
         // -> 순회할리스트, 점수를 기록할 map, 가중치  순으로 매개변수입력 (반환형 map)
         map = calculateMap(list,map,gradeWeight);
 
-        // 리뷰 많은순 처리 필요
+        // 리뷰 많은순 처리
         list = sortByReviews(list);
         map = calculateMap(list,map,reviewWeight);
 
-        // 가격 낮은순으로 처리 필요
+        // 가격 낮은순으로 처리
         list = sortByPrice(list);
         map = calculateMap(list,map,priceWeight);
 
-        // 거리 순으로 처리 필요
+        // 거리 순으로 처리
         list = sortByDistance(list,new xyPoint(37.489552f,126.724072f));
         map = calculateMap(list,map,distanceWeight);
-
 
         // 처리된 결과에 따라 list 재정렬
         HashMap<Store, Integer> finalMap = map;
@@ -58,7 +55,6 @@ public class SortMethod {
                 return finalMap.get(o2)- finalMap.get(o1);
             }
         });
-
         return list;
     }
 
@@ -113,14 +109,14 @@ public class SortMethod {
     }
 
     public List<Store> sortByPrice(List<Store> list,Object...flag){
-        final int orderDirection = (flag.length==0?-1:1); //내림차순일 때 -1, 오름차순일때 1
+        final int orderDirection = (flag.length==0?1:-1); // 오름차순일때 1,내림차순일 때 -1
 
         if(orderDirection==-1) //내림차순으로 정렬할때는 (각 매장당) 비싼가격이 높은것이 먼저 보이게 정렬
             list.sort((o1,o2)->{
                 int src1 = o1.getHighprice();
                 int src2 = o2.getHighprice();
                 if(src1-src2!=0){
-                    return (src1>src2?-1:1)*orderDirection;
+                    return (src1>src2?1:-1)*orderDirection;
                 }else
                     return 0;
             });
@@ -129,7 +125,7 @@ public class SortMethod {
                 int src1 = o1.getLowprice();
                 int src2 = o2.getLowprice();
                 if(src1-src2!=0){
-                    return (src1<src2?1:-1)*orderDirection;
+                    return (src1<src2?-1:1)*orderDirection;
                 }else
                     return 0;
             });
@@ -138,7 +134,7 @@ public class SortMethod {
     }
 
     public List<Store> sortByDistance(List<Store> list,xyPoint point,Object...flag){ //
-        final int orderDirection = (flag.length==0?1:-1); //오름차순일 때 -1, 내림차순일때 1
+        final int orderDirection = (flag.length==0?-1:1); //오름차순일 때 -1, 내림차순일때 1
         float lat = point.getLat();
         float lng = point.getLng();
 
