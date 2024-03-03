39319d32f5fc77236868dae039c1a7b7bafdfaee
Irwin-Kr <xogud717@daum.net>
2024-03-03 09:25:30 +0900

    이미지 저장 방식 변경

--- src/main/java/com/itac/login/service/StoreService.java
+++ src/main/java/com/itac/login/service/StoreService.java
@@ -26,7 +26,7 @@ public class StoreService {
     // 파일 저장 위치
     // 윈도우
     //private final String imageDir = "c:/temp";
-    private final String imageDir = "/temp";
+    private final String imageDir = "/hoe/web2/data";
 
     //멤버
     private final UserRepository userRepo;
@@ -135,11 +135,11 @@ public class StoreService {
             for (MultipartFile file : files) {
                 UUID uuid = UUID.randomUUID();
                 String imageName = uuid + "_" + file.getOriginalFilename();
-                String filePath = imageDir + "/" + uuid.toString().charAt(0) + "/";
+                String filePath = imageDir + "/";
                 File folder = new File(filePath);
                 if (!folder.exists()) {
                     try {
-                        folder.mkdir();
+                        folder.mkdirs();
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
